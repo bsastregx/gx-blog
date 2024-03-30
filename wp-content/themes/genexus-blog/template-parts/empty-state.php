048b11a7e6e3b5e79462aa6691e599feeafc28ec
Bruno Sastre <bsastre@genexus.com>
2024-03-30 20:10:17 -0300

    add empty-state

---
+++ wp-content/themes/genexus-blog/template-parts/empty-state.php
@@ -0,0 +1,10 @@
+<?php
+    $caption = $args[0];
+?>
+
+<article class="empty-state">
+    <h1 class="h3 featured-post__title">
+        No <?php echo $caption ?> to display 😑
+    </h1>
+</article>
+<hr class="post-separator post-separator--mobile">
